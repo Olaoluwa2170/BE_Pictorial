18464a76a4e0434af7e292116cfa4c427be49c26
Babalola Elisha <elitebabson@gmail.com>
2024-01-27 18:53:23 +0100

    style

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom"
 const RootLayout = () => {
   return (
     <>
-      <div className="w-full md:flex h-screen overflow-hidden">
+      <div className="w-full md:flex h-full overflow-hidden">
         <Topbar/>
         <div className="custom-scrollbar overflow-auto">
           <LeftSidebar />
